8bfeb5e31eb19381e722197607ac63ee5d392597
caiohbe <caiohbe@gmail.com>
2022-11-09 16:26:58 -0300

    get participants

--- index.js
+++ index.js
@@ -7,4 +7,28 @@ const app = express()
 // Configs
 app.use(cors())
 app.use(express.json())
-const mongoClient = new MongoClient("mongodb://")
+const mongoClient = new MongoClient("mongodb://localhost:27017")
+let db
+
+mongoClient.connect()
+    .then(() => {
+    db = mongoClient.db("")
+    })
+    .catch(err => console.log(err))
+
+app.get('/participants', (req, res) => {
+    db.collection("participants")
+    .find()
+    .toArray()
+    .then((participants) => {
+        res.send(participants)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.sendStatus(500)
+    })
+})
+
+app.listen(4000, () => {
+    console.log(`Server running in port: ${4000}`);
+  });
