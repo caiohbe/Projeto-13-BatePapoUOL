40a71f659ea655bb2f8cbf9d53b3e2eb9e7a74a2
caiohbe <caiohbe@gmail.com>
2022-11-11 14:20:47 -0300

    post messages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { query } from 'express'
 import cors from 'cors'
 import joi from 'joi'
 import { MongoClient } from 'mongodb'
@@ -23,15 +23,10 @@ const userSchema = joi.object({
     name: joi.string().required(),
 })
 
-app.get('/participants', async (req, res) => {
-
-    try {
-        const participants = await db.collection("participants").find().toArray()
-        res.send(participants)
-    } catch (err) {
-        console.log(err)
-        res.sendStatus(500)
-    }
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required(),
 })
 
 app.post('/participants', async (req, res) => {
@@ -70,8 +65,75 @@ app.post('/participants', async (req, res) => {
         db.collection("messages").insertOne(message)
         res.sendStatus(201)
     } catch (err) {
-        res.sendStatus(500).send(err)
+        res.status(500).send(err)
     }    
+})
+
+app.get('/participants', async (req, res) => {
+    try {
+        const participants = await db.collection("participants").find().toArray()
+        res.send(participants)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+app.post('/messages', async (req, res) => {
+    const { to, text, type } = req.body
+    const from = req.headers.user
+    console.log(from)
+
+    const validation = messageSchema.validate(req.body, {abortEarly: false})
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        res.status(422).send(errors)
+        return
+    }
+
+    if (type !== 'message' && type !== 'private_message') {
+        res.status(422).send('"type" inválido')
+        return
+    }
+
+    try {
+        const participants = await db.collection("participants").find().toArray()
+        if (!participants.find(p => p.name === from)) {
+            res.status(422).send('Participante não existente na lista de participantes')
+        }
+        
+    } catch (err) {
+        console.log(err)
+    }
+
+    try {
+        const message = {
+            from,
+            to,
+            text,
+            type,
+            time: dayjs(Date.now()).format('HH:mm:ss')
+        }
+
+        db.collection("messages").insertOne(message)
+        res.status(200).send('criou')
+    } catch (err) {
+        
+    }
+})
+
+app.get('/messages', async (req, res) => {
+    const { limit } = req.query
+    console.log(limit)
+
+    try {
+        const messages = await db.collection("messages").find().toArray()
+        res.send(messages)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
 app.listen(5000, () => {
