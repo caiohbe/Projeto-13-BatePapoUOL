5112cafc9b12636aaed98fd2b754c3f851f77507
caiohbe <caiohbe@gmail.com>
2022-11-11 09:54:52 -0300

    insert message on post participant + dayjs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import joi from 'joi'
 import { MongoClient } from 'mongodb'
+import dayjs from 'dayjs'
 
 const app = express()
 
@@ -20,26 +21,23 @@ try {
 
 const userSchema = joi.object({
     name: joi.string().required(),
-    lastStatus: joi.number().required()
 })
 
-app.get('/participants', (req, res) => {
-    db.collection("participants")
-    .find()
-    .toArray()
-    .then((participants) => {
+app.get('/participants', async (req, res) => {
+
+    try {
+        const participants = await db.collection("participants").find().toArray()
         res.send(participants)
-    })
-    .catch((err) => {
+    } catch (err) {
         console.log(err)
         res.sendStatus(500)
-    })
+    }
 })
 
 app.post('/participants', async (req, res) => {
-    const user = req.body
+    const body = req.body
 
-    const validation = userSchema.validate(user, {abortEarly: false})
+    const validation = userSchema.validate(body, {abortEarly: false})
 
     if (validation.error) {
         const errors = validation.error.details.map(detail => detail.message)
@@ -49,13 +47,27 @@ app.post('/participants', async (req, res) => {
     
     try {
         const participants = await db.collection("participants").find().toArray()
-        const found = participants.find((p) => p.name === user.name)
+        const found = participants.find((p) => p.name === body.name)
         if (found) {
             res.status(409).send({ error: "Usuário já existe." })
             return
         }
 
+        const user = {
+            name: body.name,
+            lastStatus: Date.now()
+        }
+
+        const message = {
+            from: body.name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: dayjs(Date.now()).format('HH:mm:ss')
+        }
+
         db.collection("participants").insertOne(user)
+        db.collection("messages").insertOne(message)
         res.sendStatus(201)
     } catch (err) {
         res.sendStatus(500).send(err)
